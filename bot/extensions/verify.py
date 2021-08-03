bd8bc3f928befaff17c65210ccfec299d8c6fba6
spooky-book <lidaniel.sb@gmail.com>
2021-08-03 20:01:36 +1000

    made the verification system fully functional, bugs may remain not sure if it is fixed

--- bot/extensions/verify.py
+++ bot/extensions/verify.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 import asyncio
 import smtplib
@@ -18,73 +19,87 @@ class verification(commands.Cog):
         load_dotenv()
         self.bot = bot
 
+    '''
+        Authenticates a user using their zID to ensure that they are part of UNSW
+        This is only the automatic verification system, if other people want to join they can manually ask FINSOC committee members
+
+        Asks use for their zID, creates and sends a verification code via email to the individuals UNSW email.
+        Bot will use this verification code to verify the individual and grant them the VERIFIED role in the FINSOC Guild
+    '''
     @commands.command(brief="This commands helps you get verified for this server")
     async def verify(self, ctx, arg=None):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             await ctx.send("Hello, I will help you get verified on the server.")
             await ctx.send("Please enter your zID without the z")
 
-            def is_zID_correct(message):
-                if not(ctx.author == message.author and ctx.channel == message.channel):
-                    return False
-
-                id = message.content.strip()
-
-                if id.isdigit() and len(id) == 7:
+            def not_bot(message):
+                if ctx.author == message.author and ctx.channel == message.channel:
                     return True
                 else:
-                    # ctx.send("Make sure you have entered your zID correctly")
                     return False
-
-            def is_OTP_correct(message):
-                if not(ctx.author == message.author and ctx.channel == message.channel):
-                    return False
-                else:
-                    return True
 
             # there is currently a bug im pretty sure in which the client.wait for picks up the bots own message and uses that
-            # probs need some checks or something idk not sure if its actually fixed
 
-            # i also currently need to create an error message when the input is wrong however it shouldnt end the command i.e. we should be able to continue entering zid without writing .verify
+            # TODO maybe be able to continue entering zid without writing .verify
 
             try:
-                ID = await self.bot.wait_for('message', check=is_zID_correct, timeout=15.0)
-                await ctx.send("Thanks, we will send an verification code to your UNSW email, please just reply to me with your verification code. There will be a 2 minute timer for the verification code")
-                print(ID.content)
-                print(ID)
+                message = await self.bot.wait_for('message', check=not_bot, timeout=15.0)
+                zID = message.content.strip()
+
+                if zID.isdigit() and len(zID) == 7:
+                    await ctx.send("Thanks, we will send an verification code to your UNSW email, please just reply to me with your verification code. There will be a 2 minute timer for the verification code")
+
+                else:
+                    await ctx.send("Make sure you have entered your zID correctly. Please restart process to try again.")
+                    return
+
+
+                print("Message Content (zID):", message.content)
+                print("Message Author:", message.author)
 
                 OTP = self.create_verification_code()
 
-                email_successful = self.send_email(ID, OTP)
+                email_successful = self.send_email(zID, OTP)
 
                 if email_successful:
-                    password = await self.bot.wait_for('message', timeout=120.0)
-
+                    password = await self.bot.wait_for('message', check=not_bot, timeout=120.0)
                     if OTP == password.content.strip():
-                        # insert verification code here
+                        # grabs the UNSW finsoc server using guildID
+                        guild_ID = int(os.getenv("FINSOC_GUILD_ID"))
+
+                        myguild = self.bot.get_guild(guild_ID)
+                        role = myguild.get_role(869547973095333928)
+
+                        member = await myguild.fetch_member(password.author.id)
+
+                        await member.add_roles(role, reason="passed verification system")
                         await ctx.send("Verified, you can now interact on the server")
+
                     else:
                         await ctx.send("Verification Code not accepted please repeat the process to get verified.")
+
                 else:
                     await ctx.send("Something went wrong when sending your verification code, please try again. If this is recurring try waiting before contacting IT")
 
             except asyncio.TimeoutError:
                 await ctx.send("You took too long to respond. If you still want to verify your status on the server please use the verify command again. Thanks.")
+
             except Exception as e:
                 print(e)
+                await ctx.send("Something went wrong, please try again. If this keeps happening please wait then contact FINSOC IT. Thank you. :)")
+
         else:
             await ctx.send("This command only works in a DM, please message the bot directly to get verified.")
 
-    # we need to be able to create a blacklist that prevents people from reverification after banning
-    # they will have to directly message execs to get unblacklisted
-    # might not be top priority
+    # TODO we need to be able to create a blacklist that prevents people from reverification after banning
+    # they will have to directly message execs to get unblacklisted might not be top priority
     def send_email(self, zID, OTP):
         try:
             server = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465, context=ssl.create_default_context())
 
             password = os.getenv("finsoc_bot_pass")
             server.login("unswfinsocbot@gmail.com", password)
-            receiver_email = 'z' + zID.content + "@ad.unsw.edu.au"
+            receiver_email = 'z' + zID + "@ad.unsw.edu.au"
 
             message = MIMEMultipart('alternative')
 
@@ -126,7 +141,8 @@ class verification(commands.Cog):
             server.sendmail("unswfinsocbot@gmail.com", receiver_email, message.as_string())
 
             return True
-        except Exception:
+        except Exception as email_exception:
+            print(email_exception)
             return False
         finally:
             server.close()
